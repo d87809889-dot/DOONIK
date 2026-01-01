c0cde664a89c44aa8eefcf6de2f7881c82f78afc
d87809889-dot <d87809889@gmail.com>
2026-01-01 18:02:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -196,3 +196,4 @@ if uploaded_file:
             doc.save(bio)
             st.download_button("📥 WORDDA YUKLAB OLISH", bio.getvalue(), "academic_report.docx", key="word_down")
             st.balloons()
+
